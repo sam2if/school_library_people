9935a2cdcab8b72cb7eaba6c891b712a57dcc175
Samuel <samif3443@gmail.com>
2023-12-08 11:41:04 -0800

    modify teacher files

--- teacher.rb
+++ teacher.rb
@@ -1,13 +1,15 @@
 require_relative 'person'
+
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(id, age, specialization, name = 'unknown', parent_permission: true)
-    super(id, age, name, parent_permission)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
-  def can_use_services?
+  def can_use_services
+    super
     true
   end
 end
