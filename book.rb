9b3eb428bc2e3f935819f4e2da3659602a581515
Samuel <samif3443@gmail.com>
2023-12-07 22:35:32 -0800

    Fix linter errors

--- book.rb
+++ book.rb
@@ -1,14 +1,13 @@
 class Book
-    attr_accessor :author, :title, :rentals
-  
-    def initialize(title, author)
-      @title = title
-      @author = author
-      @rentals = []
-    end
-  
-    def add_rental(rental)
-      @rentals.push(rental)
-    end
+  attr_accessor :author, :title, :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
   end
-  
+
+  def add_rental(rental)
+    @rentals.push(rental)
+  end
+end
